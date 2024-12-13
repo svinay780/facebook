4ea621d13ae16322ad9257ab8885d2b32868cdd2
Vinay Singh <svinay780@gmail.com>
2024-12-13 22:32:16 +0530

    monogo server

--- init.js
+++ init.js
@@ -7,7 +7,7 @@ main()
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/facebook');
+  await mongoose.connect('mongodb+srv://svinay780:7QEeUG57.At2Wug@facebook.pjmgl.mongodb.net/?retryWrites=true&w=majority&appName=Facebook');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
